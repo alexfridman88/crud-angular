84c7598014cf18b82ebaad57c81c3eaa05c0c119
Alex <49320519+alexfridman88@users.noreply.github.com>
2019-06-28 13:21:19 +0300

    Update environment.prod.ts
    deleted Fire-base connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   //Firebase settings
   firebase:{
-    apiKey: "AIzaSyBqBwUEvVh5VZpyqaMgOAPqgC8KlKMeI1I",
-    authDomain: "crud-b8282.firebaseapp.com",
-    databaseURL: "https://crud-b8282.firebaseio.com",
-    projectId: "crud-b8282",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "373980856774",
-    appId: "1:373980856774:web:53b5d89c8c7c7aea"
+    messagingSenderId: "",
+    appId: ""
   }
 };
